516da13e4087a452aebbf7e2b4f2029d1a3b355e
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-08-07 12:41:05 +0330

    add needed files

--- note.java
+++ note.java
@@ -5,4 +5,24 @@ b anha dastresi dashte bashim va masalan backup anha ra dar server digar zakhire
 agar in 4 directiory ra dashte bashim baghiyeye project mohem nist va ba dashtan in 4 directory agar
 moshkeli baraye project rokh dahad mitavanim in 4 directory ra bardashte va project ra dobare run mikonim
 
+--python manage.py collectstatic '--noinput'
+/noinput/ baes mishavad k agar bad az command niyaz b confirm bod masalan yes niyaz bod bznim khodesh handel konad va yes bzanad
+"noinput" ro dar hameye sharayet behtar ast bzarim baraye ehtiyat
+
+"create superuser"
+bayad username va email va password ra dar command khod bdahim
+baraye dadan password bayad az command zir estefade konim va password ra zakhire konim
+
+--export DJANGO_SUPERUSER_PASSWORD=password
+--python manage.py createsuperuser --noinput --user admin --email admin@gmail.com
+
+user name va email ra mostaghiman dar command minevisim va '--noinput' ham password ra az moteghayere mohiti k
+zakhire karde bodim migirad va baraye password migozarad
+
+
+"default.conf"
+yek file hast k config haye asli nginx dar an gharar migirand va bayad dar file nginx sakhte shavad
+
+
+"docker-compose.yml"
 
