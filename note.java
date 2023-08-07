f55637eba6f23e6842a57bc8fea23d68470a296d
Reza Mahmoudian <rezamahmoudian80@gmail.com>
2023-08-07 10:41:02 +0330

    add static and media files settings

---
+++ note.java
@@ -0,0 +1,8 @@
+
+
+"app media static dbdata"(etelaat databse) bayad dar directorihaye khas zakhire shavand chon gahi oghat niyaz ast
+b anha dastresi dashte bashim va masalan backup anha ra dar server digar zakhire konim,...
+agar in 4 directiory ra dashte bashim baghiyeye project mohem nist va ba dashtan in 4 directory agar
+moshkeli baraye project rokh dahad mitavanim in 4 directory ra bardashte va project ra dobare run mikonim
+
+
